55b715f4e0109724a511bf87db2aad593cd4c137
Amartya Roy <amartyaroy50@gmail.com>
2020-11-07 13:01:57 +0530

    Add loss function and optimizations step
    optimization is done using GradientTape

--- Style_Transfer.py
+++ Style_Transfer.py
@@ -163,3 +163,37 @@ image = tf.Variable(content_image)
 
 def clip_0_1(image):
   return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+
+
+opt = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
+
+
+
+style_weight=1
+content_weight=1e4
+
+def style_content_loss(outputs):
+    style_outputs = outputs['style']
+    content_outputs = outputs['content']
+    style_loss = tf.add_n([tf.reduce_mean(
+        
+        (style_outputs[name]-style_targets[name])**2
+        
+        )      for name in style_outputs.keys()])
+    style_loss *= style_weight / num_style_layers
+
+    content_loss = tf.add_n([tf.reduce_mean((content_outputs[name]-content_targets[name])**2) 
+                             for name in content_outputs.keys()])
+    content_loss *= content_weight / num_content_layers
+    loss = style_loss + content_loss
+    return loss
+
+@tf.function()
+def train_step(image):
+  with tf.GradientTape() as tape:
+    outputs = extractor(image)
+    loss = style_content_loss(outputs)
+
+  grad = tape.gradient(loss, image)
+  opt.apply_gradients([(grad, image)])
+  image.assign(clip_0_1(image))
