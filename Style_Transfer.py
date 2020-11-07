1abbe2a1a0e4fb021e6a980abf0d92cc3ecfd8b5
Amartya Roy <amartyaroy50@gmail.com>
2020-11-07 12:39:46 +0530

    Add gram matrix function
    the function was implemented using einstein notation

--- Style_Transfer.py
+++ Style_Transfer.py
@@ -96,3 +96,8 @@ def vgg_layers(layer_names):
   model = tf.keras.Model([vgg.input], outputs)
   return model
 
+def gram_matrix(input_tensor):
+  result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+  input_shape = tf.shape(input_tensor)
+  num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
+  return result/(num_locations)
