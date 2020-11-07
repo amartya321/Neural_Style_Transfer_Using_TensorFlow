23d9f387e284e6d3621f3539ef30fe23461fc50f
Amartya Roy <amartyaroy50@gmail.com>
2020-11-07 11:47:55 +0530

    Add load_img fucntion
    This function takes an image path ,read the file and perform rescaling on them

--- Style_Transfer.py
+++ Style_Transfer.py
@@ -38,3 +38,24 @@ def imshow(image, title=None):
   if title:
     plt.title(title)
 
+
+def load_img(path_to_img):
+  max_dim = 512
+  img = tf.io.read_file(path_to_img)
+  ### this one read file in the form of A Tensor of type string.
+  img = tf.image.decode_image(img, channels=3)
+  #Detects whether an image is a BMP, GIF, JPEG, or PNG, and performs the appropriate operation to convert the input bytes string into a Tensor of type dtype.
+  img = tf.image.convert_image_dtype(img, tf.float32)
+
+  #rescaling the image
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  #changing the type of shape to do floting operations on them
+  long_dim = max(shape)
+  scale = max_dim / long_dim
+
+  new_shape = tf.cast(shape * scale, tf.int32)
+
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  return img
+  
